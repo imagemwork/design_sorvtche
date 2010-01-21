c03a33b773bd21ac82663ea11c3f4d3d328a10a8
edipofederle <edipofederle@gmail.com>
2010-01-21 15:17:27 -0200

    Algumas coias relacionadas a performace do font-end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,17 @@ module ApplicationHelper
 	def selected(name)
 		content_for(name) { "selected" }	
 	end
+	
+	def stylesheets(*files)
+	 content_for(:stylesheets) { stylesheet_link_merged(*files)}
+	end
+	
+	def javascripts_normal(*files)
+	  content_for(:javascripts_normal) { javascript_include_tag(*files) }	 
+	end
+	
+	def javascripts(*files)
+	 content_for(:javascripts) {javascript_include_merged(*files)}
+	end
 
 end
